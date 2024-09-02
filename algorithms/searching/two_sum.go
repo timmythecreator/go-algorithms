e72c94c05bc08ec9b4bcfc8b4adc777d20f56cb5
Timmythecreator <timurledos@gmail.com>
2024-09-03 01:19:52 +0200

    Changing implementation to a more optimized one

--- algorithms/searching/two_sum.go
+++ algorithms/searching/two_sum.go
@@ -6,13 +6,14 @@ func TwoSumSample() {
 	fmt.Print(`
 	TwoSum:
 The goal is to find two numbers in an array that add up to a given target sum.
-We use a hash map to store the numbers we have seen so far and their indices.
-For each number, we calculate its complement with respect to the target sum.
-If the complement exists in the hash map, we return the indices of the current number and its complement.
+We changed the initial version of the TwoSum function to a more optimized version.
+The new version uses two pointers to iterate over the array and find the target sum.
+Why is this version better?
+Because it doesn't use a hash map to store the numbers we have seen so far.
 	`)
-	arr := []int{2, 3, 7, 4, 5}
+	arr := []int{2, 3, 4, 5, 7}
 	target := 9
-	fmt.Println("\nSample array:", arr)
+	fmt.Println("\nSample array (sorted):", arr)
 	fmt.Println("Target sum:", target)
 	result := TwoSum(arr, target)
 	fmt.Print(`
@@ -21,9 +22,28 @@ We iterate over the array and for each number, we calculate its complement.
 If the complement exists in the hash map, we return the indices of the current number and its complement.
 And this is how it will look like:`)
 	fmt.Print(result)
-	fmt.Println("\nThe complexity of this algorithm is O(n)")
+	fmt.Println("\nThe runtime of this algorithm is O(n), the space complexity is O(1).")
 }
 
+func TwoSum(arr []int, target int) []int {
+	i, r := 0, len(arr)-1
+	for i < r {
+		sum := arr[i] + arr[r]
+		if sum == target {
+			return []int{i, r}
+		}
+		if sum < target {
+			i++
+		} else {
+			r--
+		}
+	}
+	return nil
+}
+
+// This is the first version of the TwoSum function.
+// It uses a hash map to store the numbers we have seen so far and their indices.
+/*
 func TwoSum(arr []int, target int) []int {
 	numMap := make(map[int]int)
 	for i, num := range arr {
@@ -35,3 +55,4 @@ func TwoSum(arr []int, target int) []int {
 	}
 	return nil
 }
+*/
